ca494951c08b794edbcf51d3b3667fa6172cbcc3
Marlon2025c <bastien.lecour2025@gmail>
2024-07-04 18:05:52 +0200

    🐛 Fix QSS 🐛

--- function/encrypt_script.py
+++ function/encrypt_script.py
@@ -8,9 +8,9 @@ with open("key.key", "wb") as key_file:
 fernet = Fernet(key)
 
 # Informations d'identification à crypter
-client_id = "4fa65ec1-aa5c-41b4-8714-4313b34f1cd6"
-url_redirect = "https://login.microsoftonline.com/common/oauth2/nativeclient"
-secret = "765641af-6b79-488b-b572-e846909a99a4"
+client_id = "Client ID de Votre Azure"
+url_redirect = "url_redirect de Votre Azure"
+secret = "secret de Votre Azure"
 # Afficher la clé de chiffrement et les informations d'identification cryptées
 print(f'client_id : {fernet.encrypt(client_id.encode())}')
 print(f'url_redirect : {fernet.encrypt(url_redirect.encode())}')
